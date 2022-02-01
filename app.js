291c8605dfc6f9d00f9731f396e8d7e1bf98f397
MKougia <kougia.mihkel@khk.ee>
2022-02-01 12:18:58 +0200

    Added item to data structure

--- app.js
+++ app.js
@@ -24,6 +24,23 @@ const ItemCtrl = (function(){
 		getItems: function(){
 			return data.items
 		},
+		addItem: function(name, calories){
+			let ID;
+			// Create ID
+			if(data.items.length > 0){
+				ID = data.items[data.items.length - 1].id + 1
+			} else {
+				ID = 0
+			}
+			// calories to number
+			calories = parseInt(calories);
+			// create new item
+			newItem = new Item(ID, name, calories);
+			// add to items array
+			data.items.push(newItem);
+			// return new item
+			return newItem
+		},
 		logData: function(){
 			return data
 		}
@@ -35,7 +52,10 @@ const ItemCtrl = (function(){
 const UICtrl = (function(){
 	// UI selectors
 	const UISelectors = {
-		itemList: '#item-list'
+		itemList: '#item-list',
+		itemNameInput: '#item-name',
+		itemCaloriesInput: '#item-calories',
+		addBtn: '.add-btn'
 	}
 
 	return {
@@ -55,12 +75,38 @@ const UICtrl = (function(){
 
 			// insert list items
 			document.querySelector(UISelectors.itemList).innerHTML = html;
+		},
+		getSelectors: function(){
+			return UISelectors;
+		},
+		getItemInput: function(){
+			return {
+				name: document.querySelector(UISelectors.itemNameInput).value,
+				calories: document.querySelector(UISelectors.itemCaloriesInput).value
+			}
 		}
 	}
 })();
 
 // App Controller
 const App = (function(ItemCtrl, UICtrl){
+	// Load event listeners
+	const loadEventListeners = function(){
+		// get UI selectors
+		const UISelectors = UICtrl.getSelectors()
+		// add item event
+		document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+	}
+	// item add submit function
+	const itemAddSubmit = function(event){
+		const input = UICtrl.getItemInput()
+		// check for name and calorie input
+		if(input.name !== '' && input.calories !== ''){
+			const newItem = ItemCtrl.addItem(input.name, input.calories)
+			console.log(newItem)
+		}
+		event.preventDefault()
+	}
 	return {
 		init: function(){
 			console.log('Initializing App')
@@ -68,6 +114,8 @@ const App = (function(ItemCtrl, UICtrl){
 			const items = ItemCtrl.getItems()
 			// populate items list
 			UICtrl.populateItemList(items)
+			// load event listeners
+			loadEventListeners();
 		}
 	}
 })(ItemCtrl, UICtrl);
